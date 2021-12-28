7df90d963914965b32d1dfeeb9912e382cbc6e4f
James DeLorenzo <jdelo417@gmail.com>
2021-12-28 14:48:33 -0500

    get new inputsystem working

--- roll-a-ball/Assets/Scripts/InputController.cs
+++ roll-a-ball/Assets/Scripts/InputController.cs
@@ -6,38 +6,21 @@ using UnityEngine.InputSystem;
 public class InputController : MonoBehaviour
 {
 
-    //[SerializeField] InputAction up = KeyCode.W;
-    //[SerializeField] KeyCode down = KeyCode.S;
-    //[SerializeField] KeyCode left = KeyCode.A;
-    //[SerializeField] KeyCode right = KeyCode.D;
     [SerializeField] InputAction BoardTilt;
-    [SerializeField] float maxTilt = 10;
     [SerializeField] float lerpDamping = 2;
 
     void Start()
     {
         BoardTilt.Enable();
     }
+
     void Update()
     {
-        // TODO: get new input system working
-        if(BoardTilt.enabled)
-            print(BoardTilt.ReadValueAsObject());
-        //Vector3 targetRotation = Vector3.zero;
-        //if (Input.GetKey(up))
-        //    targetRotation.x = maxTilt;
-        //if (Input.GetKey(down))
-        //    targetRotation.x = -maxTilt;
-        //if (Input.GetKey(left))
-        //    targetRotation.z = maxTilt;
-        //if (Input.GetKey(right))
-        //    targetRotation.z = -maxTilt;
-
-        //var desiredRotation = Quaternion.Euler(targetRotation.x, targetRotation.y, targetRotation.z);
-
-        //transform.rotation =  Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * lerpDamping);
-
-        //Input
+        // read input and apply tilt to the gameboard
+        var inputValue = BoardTilt.ReadValue<Vector2>();
+        Vector2 desiredTilt = inputValue != null ? inputValue : Vector2.zero;
+        var desiredRotation = Quaternion.Euler(desiredTilt.x, 0, desiredTilt.y);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * lerpDamping);
     }
 
 }
