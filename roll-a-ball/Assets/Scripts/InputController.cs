40cca20b77d2e3759bd562c3a6a9d56108c96e3a
James DeLorenzo <jdelo417@gmail.com>
2021-12-28 16:38:21 -0500

    setup URP renderer; add physics materials; update shaders

--- roll-a-ball/Assets/Scripts/InputController.cs
+++ roll-a-ball/Assets/Scripts/InputController.cs
@@ -17,9 +17,9 @@ public class InputController : MonoBehaviour
     void Update()
     {
         // read input and apply tilt to the gameboard
-        var inputValue = BoardTilt.ReadValue<Vector2>();
+        Vector2 inputValue = BoardTilt.ReadValue<Vector2>();
         Vector2 desiredTilt = inputValue != null ? inputValue : Vector2.zero;
-        var desiredRotation = Quaternion.Euler(desiredTilt.x, 0, desiredTilt.y);
+        var desiredRotation = Quaternion.Euler(desiredTilt.y, 0, -desiredTilt.x);
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * lerpDamping);
     }
 
